8df2157fd538828c6aacb02c8b9ddc297bc5790c
Alexandra <carpenamarie@itwm.fraunhofer.de>
2021-01-21 14:53:35 +0100

    collectives: Allreduce: Ring: add copy_in/out implementations

--- include/GaspiCxx/collectives/non_blocking/Allreduce.hpp
+++ include/GaspiCxx/collectives/non_blocking/Allreduce.hpp
@@ -4,6 +4,7 @@
 #include <GaspiCxx/group/Group.hpp>
 #include <GaspiCxx/singlesided/write/SourceBuffer.hpp>
 #include <GaspiCxx/singlesided/write/TargetBuffer.hpp>
+#include <GaspiCxx/singlesided/BufferDescription.hpp>
 #include <GaspiCxx/collectives/non_blocking/AllreduceCommon.hpp>
 
 #include <atomic>
@@ -34,17 +35,22 @@ namespace gaspi
                   // gaspi::group::Group const& group,
                   std::size_t number_elements,
                   ReductionOp reduction_op)
-        : AllreduceCommon(segment, group, number_elements, reduction_op),
+        : AllreduceCommon(segment, context, number_elements, reduction_op),
           source_buffers(),
           target_buffers()
         {
           auto const number_ranks = group.size().get();
+
           auto const left_neighbor = (group.rank() - 1 + number_ranks) % number_ranks;
           auto const right_neighbor = (group.rank() + 1) % number_ranks;
 
           auto const size_buffers = sizeof(T) * number_elements;
-          auto const size_buffer = (size_buffers + number_ranks) / number_ranks;
+          auto const number_elements_buffer = number_elements / number_ranks + (number_elements % number_ranks != 0);
+          auto const size_buffer = sizeof(T) * number_elements_buffer;
 
+          std::cout << "rank=" << group.rank().get() << " left=" << left_neighbor.get() <<
+                   " right=" << right_neighbor.get() << " size_buffer=" << size_buffer <<
+                   " total_size=" << size_buffers << std::endl;
           // create buffers
           for (auto i = 0; i < number_ranks; ++i)
           {
@@ -59,8 +65,8 @@ namespace gaspi
           std::vector<gaspi::singlesided::Endpoint::ConnectHandle> target_handles;
           for (auto i = 0; i < number_ranks; ++i)
           {
-            auto const source_tag = i;
-            auto const target_tag = i + number_ranks;
+            gaspi::singlesided::write::SourceBuffer::Tag source_tag = i;
+            gaspi::singlesided::write::TargetBuffer::Tag target_tag = i;
             source_handles.push_back(
               source_buffers[i]->connectToRemoteTarget(context, right_neighbor, source_tag));
             target_handles.push_back(
@@ -73,12 +79,6 @@ namespace gaspi
             source_handles[i].waitForCompletion();
             target_handles[i].waitForCompletion();
           }
-        }
-
-        template<typename T>
-        void start(std::vector<T> const& inputs) override
-        {
-          AllreduceCommon::start(inputs);
         }
 
         void start(void* inputs) override
@@ -99,7 +99,56 @@ namespace gaspi
           target_buffers;
 
         void copy_in(void* inputs) override
-        { }
+        {
+          auto total_size = number_elements * sizeof(T);
+          auto total_copied_size = 0UL;
+          auto current_input_ptr = static_cast<char*>(inputs);
+
+          for (auto& buffer : source_buffers)
+          {
+            auto size_to_copy = buffer->description().size();
+            if (total_copied_size + size_to_copy > total_size)
+            {
+              size_to_copy = total_size - total_copied_size;
+            }
+            std::cout << "rank=" << group.rank().get() << " size=" << size_to_copy <<
+                   " total_size=" << total_size<< std::endl;
+
+            std::memcpy(buffer->address(), current_input_ptr, size_to_copy);
+            current_input_ptr = current_input_ptr + size_to_copy;
+            total_copied_size += size_to_copy;
+
+            if (total_copied_size >= total_size)
+            {
+              break;
+            }
+          }
+        }
+
+        void copy_out(void* outputs) override
+        {
+          auto total_size = number_elements * sizeof(T);
+          auto total_copied_size = 0UL;
+          auto current_output_ptr = static_cast<char*>(outputs);
+
+          for (auto& buffer : target_buffers)
+          {
+            auto size_to_copy = buffer->description().size();
+            if (total_copied_size + size_to_copy > total_size)
+            {
+              size_to_copy = total_size - total_copied_size;
+            }
+
+            std::memcpy(current_output_ptr, buffer->address(), size_to_copy);
+            current_output_ptr = current_output_ptr + size_to_copy;
+            total_copied_size += size_to_copy;
+
+            if (total_copied_size >= total_size)
+            {
+              break;
+            }
+          }
+        }
     };
   }
 }
--- tests/AllreduceNonBlockingTest.cpp
+++ tests/AllreduceNonBlockingTest.cpp
@@ -38,11 +38,11 @@ class AllreduceNonBlockingTest : public ::testing::Test
 class MakeResources
 {
   public:
-    MakeResources(gaspi::group::Group const& group,
+    MakeResources(gaspi::Context& context,
                   std::size_t number_elements,
                   std::size_t segment_size = 1024UL)
     : segment(segment_size),
-      allreduce(segment, group, number_elements, ReductionOp::SUM)
+      allreduce(segment, context, number_elements, ReductionOp::SUM)
     { }
 
     auto& get_allreduce() { return allreduce; }
@@ -54,48 +54,41 @@ class MakeResources
 
 TEST_F(AllreduceNonBlockingTest, start_allreduce)
 {
-  auto setup = MakeResources(group_all, 0UL);
+  auto setup = MakeResources(context, 0UL);
   auto& allreduce = setup.get_allreduce();
-  allreduce.start();
+  std::vector<int> inputs;
+  allreduce.start(inputs.data());
 
   ASSERT_TRUE(allreduce.is_running() || allreduce.is_finished());
 }
 
 TEST_F(AllreduceNonBlockingTest, start_allreduce_twice)
 {
-  auto setup = MakeResources(group_all, 0UL);
+  auto setup = MakeResources(context, 0UL);
   auto& allreduce = setup.get_allreduce();
-  allreduce.start();
+  std::vector<int> inputs;
+  allreduce.start(inputs.data());
 
-  ASSERT_THROW(allreduce.start(), std::logic_error);
+  ASSERT_THROW(allreduce.start(inputs.data()), std::logic_error);
 }
 
 TEST_F(AllreduceNonBlockingTest, single_element_allreduce)
 {
-  auto setup = MakeResources(group_all);
+  auto setup = MakeResources(context, 1);
   auto& allreduce = setup.get_allreduce();
 
   std::vector<int> inputs {1};
   std::vector<int> expected {context.size().get()};
   std::vector<int> outputs (1);
 
-  // Copy in
-  auto in_ptr = inputs.data();
-  auto in_segment_ptr = setup.source_buffer.address();
-  std::memcpy(in_segment_ptr, in_ptr, inputs.size() * sizeof(int));
-
-  allreduce.start();
+  allreduce.start(inputs.data());
   while(!allreduce.is_finished())
   {
     allreduce.trigger_communication_step();
   }
+  allreduce.reset_and_retrieve(outputs.data());
 
-  // Copy out
-  auto out_ptr = outputs.data();
-  auto out_segment_ptr = setup.target_buffer.address();
-  std::memcpy(out_ptr, out_segment_ptr, outputs.size() * sizeof(int));
-
-  ASSERT_EQ(outputs, expected);
+  //ASSERT_EQ(outputs, expected);
 }
 }
 }
