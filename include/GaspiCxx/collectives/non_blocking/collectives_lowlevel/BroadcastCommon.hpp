987fbf845b7a8ed605b0d309691785cd1fcd21af
Alexandra <carpenamarie@itwm.fraunhofer.de>
2021-02-05 12:02:53 +0100

    collectives: Broadcast: start implementation

---
+++ include/GaspiCxx/collectives/non_blocking/collectives_lowlevel/BroadcastCommon.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <GaspiCxx/collectives/non_blocking/collectives_lowlevel/CollectiveLowLevel.hpp>
+#include <GaspiCxx/Context.hpp>
+#include <GaspiCxx/group/Group.hpp>
+#include <GaspiCxx/segment/Segment.hpp>
+
+namespace gaspi
+{
+  namespace collectives
+  {
+    enum class BroadcastAlgorithm
+    {
+      SEND_TO_ALL,
+    };
+
+    class BroadcastCommon : public CollectiveLowLevel
+    {
+      public:
+        BroadcastCommon(gaspi::segment::Segment& segment,
+                        gaspi::group::Group const& group,
+                        std::size_t number_elements,
+                        gaspi::group::Rank const& root_rank);
+
+      protected:
+        gaspi::segment::Segment& segment;
+        gaspi::group::Group const group;
+        gaspi::Context context;
+        std::size_t number_elements;
+        gaspi::group::Rank root_rank;
+    };
+
+    template<typename T, BroadcastAlgorithm Algorithm>
+    class BroadcastLowLevel : public BroadcastCommon
+    { };
+  }
+}
---
+++ src/collectives/non_blocking/collectives_lowlevel/BroadcastCommon.cpp
@@ -0,0 +1,18 @@
+#include <GaspiCxx/collectives/non_blocking/collectives_lowlevel/BroadcastCommon.hpp>
+namespace gaspi
+{
+  namespace collectives
+  {
+    BroadcastCommon::BroadcastCommon(gaspi::segment::Segment& segment, // provide this automatically from segment manager?
+                                     gaspi::group::Group const& group,
+                                     std::size_t number_elements,
+                                     gaspi::group::Rank const& root_rank)
+    : segment(segment),
+      group(group),
+      context(group),
+      number_elements(number_elements),
+      root_rank(root_rank)
+    { }
+
+  }
+}
