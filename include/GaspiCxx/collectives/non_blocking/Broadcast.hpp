3a7584099b5c2d43a2be114e1e5bbc02cc8625f6
Peter Labus <peter.labus@itwm.fraunhofer.de>
2021-02-16 10:23:37 +0100

    collectives: Broadcast: add progress engine support in high-level impl

--- include/GaspiCxx/collectives/non_blocking/Broadcast.hpp
+++ include/GaspiCxx/collectives/non_blocking/Broadcast.hpp
@@ -2,7 +2,10 @@
 
 #include <GaspiCxx/collectives/non_blocking/Collective.hpp>
 #include <GaspiCxx/collectives/non_blocking/collectives_lowlevel/BroadcastCommon.hpp>
+#include <GaspiCxx/progress_engine/ProgressEngine.hpp>
+#include <GaspiCxx/Runtime.hpp>
 
+#include <memory>
 #include <stdexcept>
 #include <vector>
 
@@ -18,7 +21,13 @@ namespace gaspi
         Broadcast(gaspi::segment::Segment& segment,
                   gaspi::group::Group const& group,
                   std::size_t number_elements,
+                  gaspi::group::Rank const& root_rank,
+                  gaspi::progress_engine::ProgressEngine& progress_engine);
+        Broadcast(gaspi::segment::Segment& segment,
+                  gaspi::group::Group const& group,
+                  std::size_t number_elements,
                   gaspi::group::Rank const& root_rank);
+        ~Broadcast();
 
         void start(void const* inputs) override;
         void start(std::vector<T> const& inputs);
@@ -28,22 +37,46 @@ namespace gaspi
         void waitForCompletion(std::vector<T>& output);
 
       private:
-        BroadcastLowLevel<T, Algorithm> broadcast_impl;
+        progress_engine::ProgressEngine& progress_engine;
+        progress_engine::ProgressEngine::CollectiveHandle handle;
+        std::shared_ptr<BroadcastLowLevel<T, Algorithm>> broadcast_impl;
+
         gaspi::group::Rank root_rank;
         gaspi::group::Rank rank;
     };
 
     template<typename T, BroadcastAlgorithm Algorithm>
-    Broadcast<T, Algorithm>::Broadcast(gaspi::segment::Segment& segment,
-                                       gaspi::group::Group const& group,
-                                       std::size_t number_elements,
-                                       gaspi::group::Rank const& root_rank)
-    : broadcast_impl(segment, group, number_elements, root_rank),
+    Broadcast<T, Algorithm>::Broadcast(
+      gaspi::segment::Segment& segment,
+      gaspi::group::Group const& group,
+      std::size_t number_elements,
+      gaspi::group::Rank const& root_rank,
+      gaspi::progress_engine::ProgressEngine& progress_engine)
+    : progress_engine(progress_engine),
+      handle(),
+      broadcast_impl(std::make_shared<BroadcastLowLevel<T, Algorithm>>(
+        segment, group, number_elements, root_rank)),
       root_rank(root_rank),
       rank(group.rank())
     {
-      broadcast_impl.waitForSetup();
-      // TODO here register with the progress engine
+      broadcast_impl->waitForSetup();
+      handle = progress_engine.register_collective(broadcast_impl);
+    }
+
+    template<typename T, BroadcastAlgorithm Algorithm>
+    Broadcast<T, Algorithm>::Broadcast(
+      gaspi::segment::Segment& segment,
+      gaspi::group::Group const& group,
+      std::size_t number_elements,
+      gaspi::group::Rank const& root_rank)
+    : Broadcast(segment, group, number_elements, root_rank,
+                gaspi::getRuntime().getDefaultProgressEngine())
+    { }
+
+    template<typename T, BroadcastAlgorithm Algorithm>
+    Broadcast<T, Algorithm>::~Broadcast()
+    {
+      progress_engine.deregister_collective(handle);
     }
 
     template<typename T, BroadcastAlgorithm Algorithm>
@@ -54,8 +87,8 @@ namespace gaspi
         throw std::logic_error(
           "Broadcast: start(void const* inputs) may only be called on root rank.");
       }
-      broadcast_impl.copyIn(inputs);
-      broadcast_impl.start();
+      broadcast_impl->copyIn(inputs);
+      broadcast_impl->start();
     }
 
     template<typename T, BroadcastAlgorithm Algorithm>
@@ -71,15 +104,15 @@ namespace gaspi
       {
         throw std::logic_error("Broadcast: start() may only be called on non-root ranks.");
       }
-      broadcast_impl.copyIn(CollectiveLowLevel::NO_DATA);
-      broadcast_impl.start();
+      broadcast_impl->copyIn(CollectiveLowLevel::NO_DATA);
+      broadcast_impl->start();
     }
 
     template<typename T, BroadcastAlgorithm Algorithm>
     void Broadcast<T, Algorithm>::waitForCompletion(void* output)
     {
-      broadcast_impl.waitForCompletion();
-      broadcast_impl.copyOut(output);
+      broadcast_impl->waitForCompletion();
+      broadcast_impl->copyOut(output);
     }
 
     template<typename T, BroadcastAlgorithm Algorithm>
