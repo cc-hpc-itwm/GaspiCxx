9e2238f7b84577a5c8d1fba544ab6dc87dca437f
Alexandra <carpenamarie@itwm.fraunhofer.de>
2021-02-22 14:22:14 +0100

    ProgressEngine: separate pure virtual `ProgressEngine` base class from implementation
    * update `RoundRobinDedicatedThread` to sleep when
      there are no operators to execute

--- src/progress_engine/RoundRobinDedicatedThread.cpp
+++ src/progress_engine/RoundRobinDedicatedThread.cpp
@@ -5,13 +5,18 @@ namespace gaspi
   namespace progress_engine
   {
     RoundRobinDedicatedThread::RoundRobinDedicatedThread()
-    : terminate_man_thread(false),
+    : current_handle(0UL),
+      terminate_man_thread(false),
       management_thread(&RoundRobinDedicatedThread::generate_progress, this)
     {}
 
     RoundRobinDedicatedThread::~RoundRobinDedicatedThread()
     {
-      terminate_man_thread = true;
+      {
+        std::lock_guard<std::mutex> lock(operators_mutex);
+        terminate_man_thread = true;
+      }
+      condition.notify_one();
       if (management_thread.joinable())
       {
         management_thread.join();
@@ -22,11 +27,48 @@ namespace gaspi
     {
       while (!terminate_man_thread)
       {
-        std::lock_guard<std::mutex> const lock(operators_mutex);
-        for (auto& handle_and_operator : operators)
+        std::unique_lock<std::mutex> lock(operators_mutex);
+        condition.wait(lock, [&done = terminate_man_thread,
+                              &operators = operators]
+                              {
+                                return (operators.size()>0) || done;
+                              });
+        if (!terminate_man_thread)
         {
-          handle_and_operator.second->triggerProgress();
+          for (auto& handle_and_operator : operators)
+          {
+            handle_and_operator.second->triggerProgress();
+          }
         }
+        lock.unlock();
+      }
+    }
+
+    ProgressEngine::CollectiveHandle
+    RoundRobinDedicatedThread::register_collective(
+              std::shared_ptr<collectives::CollectiveLowLevel> collective)
+    {
+      {
+        std::lock_guard<std::mutex> const lock(operators_mutex);
+        ++current_handle;
+        bool const is_okay = operators.insert({current_handle, collective}).second;
+        if(!is_okay)
+        {
+          throw std::logic_error("ProgressEngine: Handle has already been used.");
+        }
+      }
+      condition.notify_one();
+      return (current_handle);
+    }
+
+    void RoundRobinDedicatedThread::deregister_collective(
+                                    CollectiveHandle const& handle)
+    {
+      std::lock_guard<std::mutex> const lock(operators_mutex);
+      auto const count = operators.erase(handle);
+      if(count != 1)
+      {
+        throw std::logic_error("ProgressEngine: Could not remove operator");
       }
     }
   }
