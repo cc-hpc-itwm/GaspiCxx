afa3e16780d4993047cf01a6f212efed4254a82d
Alexandra <carpenamarie@itwm.fraunhofer.de>
2021-04-11 15:21:31 +0200

    tests: collectives: utilities for parameterized tests
    * wrapper to create objects with a specific type `select_factory_type`
    * template `Data` class to wrap `std::vector`
    * generic factory for data stored as vectors

---
+++ tests/parametrized_test_utilities.hpp
@@ -0,0 +1,88 @@
+#include <algorithm>
+#include <vector>
+
+using DataSize = std::size_t;
+using ElementType = std::string;
+
+template<template <typename> class Factory>
+auto select_factory_by_type(ElementType element_type)
+{
+  if (element_type == "int") return &Factory<int>::factory;
+  if (element_type == "float") return &Factory<float>::factory;
+  if (element_type == "double") return &Factory<double>::factory;
+
+  throw std::runtime_error("[select_factory_type] Unknown element type \"" + element_type + "\"");
+}
+
+class BaseData
+{
+  public:
+    virtual ~BaseData() {};
+    virtual void* get_data() = 0;
+
+    virtual std::size_t get_num_elements() = 0;
+    virtual void fill(double value) = 0;
+    virtual void fill_with_list(std::vector<double> const& values) = 0;
+
+    bool operator==(BaseData const& bd) const
+    {
+      return is_equal(bd);
+    }
+
+  private:
+    virtual bool is_equal(BaseData const& bd) const = 0;
+};
+
+template<typename T>
+class Data : public BaseData
+{
+  public:
+    Data(DataSize size)
+    : data(size)
+    {}
+    Data(Data const&) = default;
+    ~Data() = default;
+
+    void* get_data() override
+    {
+      return data.data();
+    }
+
+    std::size_t get_num_elements() override
+    {
+      return data.size();
+    }
+
+    void fill(double value) override
+    {
+      std::fill(data.begin(), data.end(), static_cast<T>(value));
+    }
+
+    void fill_with_list(std::vector<double> const& values) override
+    {
+      assert(data.size() == values.size());
+      std::transform(values.begin(), values.end(), data.begin(),
+                    [](double value) { return static_cast<T>(value);});
+    }
+
+  private:
+    bool is_equal(BaseData const& bd) const override
+    {
+      auto* other_data = dynamic_cast<Data const*>(&bd);
+      return data == other_data->data;
+    }
+
+    std::vector<T> data;
+};
+
+template<typename T>
+class DataFactory
+{
+  public:
+    static auto factory(DataSize size)
+    {
+      std::unique_ptr<BaseData> d = std::make_unique<Data<T>>(size);
+      return d;
+    }
+};
+
