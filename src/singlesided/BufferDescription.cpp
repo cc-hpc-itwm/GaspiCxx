4ccb6543ce11925b812fca132c8d6c3b5630b391
Daniel Grünewald <daniel.gruenewald@itwm.fhg.de>
2021-05-12 21:49:05 +0200

    update copyright year

--- src/singlesided/BufferDescription.cpp
+++ src/singlesided/BufferDescription.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Fraunhofer ITWM - <http://www.itwm.fraunhofer.de/>, 2019
+ * Copyright (c) Fraunhofer ITWM - <http://www.itwm.fraunhofer.de/>, 2019 - 2021
  *
  * This file is part of GaspiCxx.
  *
