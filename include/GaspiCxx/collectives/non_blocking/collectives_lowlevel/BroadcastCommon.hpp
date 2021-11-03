341a5da9ff656732060edb33d7f5a963a4f6f3ac
Alexandra <carpenamarie@itwm.fraunhofer.de>
2021-11-03 16:15:01 +0100

    collectives: common: add `virtual` destructors to base classes
    # Conflicts:
    #	include/GaspiCxx/collectives/non_blocking/collectives_lowlevel/AllreduceCommon.hpp
    #	include/GaspiCxx/collectives/non_blocking/collectives_lowlevel/BroadcastCommon.hpp

--- include/GaspiCxx/collectives/non_blocking/collectives_lowlevel/BroadcastCommon.hpp
+++ include/GaspiCxx/collectives/non_blocking/collectives_lowlevel/BroadcastCommon.hpp
@@ -40,6 +40,7 @@ namespace gaspi
         BroadcastCommon(gaspi::group::Group const& group,
                         std::size_t number_elements,
                         gaspi::group::Rank const& root);
+        virtual ~BroadcastCommon() = default;
 
       protected:
         gaspi::group::Group group;
