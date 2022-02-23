a0ec527342f4780732ac2a612e365a5737825b4e
Alexandra <carpenamarie@itwm.fraunhofer.de>
2022-02-23 18:07:05 +0100

    tests: pygpi: allreduce: reorganize tests to test all algorithms against main dtypes (`float`, `int`, `bool`)

--- tests/python/allreduce.py
+++ tests/python/allreduce.py
@@ -7,41 +7,24 @@ class TestAllreduce:
 
   @pytest.mark.parametrize("list_length", [0, 1, 23, 1000])
   @pytest.mark.parametrize("dtype", [float, int])
-  def test_list_identical_inputs(self, list_length, dtype):
-    input_list = [ dtype(3.456) ] * list_length
+  @pytest.mark.parametrize("algorithm", ["ring", "recursivedoubling"])
+  def test_list_algorithms(self, list_length, dtype, algorithm):
+    input_list = [ dtype(3.456), dtype(100), dtype(-4) ] * list_length
     expected_output_array = [ elem * pygpi.get_size() for elem in input_list]
 
     allreduce = pygpi.Allreduce(pygpi.Group(), len(input_list), pygpi.ReductionOp.SUM,
-                                dtype = dtype)
+                                dtype = dtype, algorithm = algorithm)
     allreduce.start(input_list)
     output_array = allreduce.wait_for_completion()
 
     assert isinstance(output_array, np.ndarray)
     assert np.array_equal(output_array, expected_output_array)
 
-  @pytest.mark.parametrize("array_length", [0, 8, 35, 67002])
-  @pytest.mark.parametrize("dtype", [np.float, np.double, np.float32, float,
-                                     np.int32, np.int16, np.int8, int])
-  def test_array_different_inputs(self, array_length, dtype):
-    input_array = np.empty(shape=(array_length), dtype=dtype)
-    input_array.fill(pygpi.get_rank())
-
-    expected_output_array = np.empty(input_array.shape, dtype=dtype)
-    expected_output_array.fill(sum(range(pygpi.get_size())))
-
-    allreduce = pygpi.Allreduce(pygpi.Group(), input_array.size, pygpi.ReductionOp.SUM,
-                                dtype = dtype)
-    allreduce.start(input_array)
-    output_array = allreduce.wait_for_completion()
-
-    assert isinstance(output_array, np.ndarray)
-    assert output_array.dtype == dtype
-    assert np.array_equal(output_array, expected_output_array)
-
   @pytest.mark.parametrize("array_length", [1, 10, 13])
   @pytest.mark.parametrize("dtype", [bool])
   @pytest.mark.parametrize("reduction_op", [pygpi.ReductionOp.AND, pygpi.ReductionOp.OR])
-  def test_array_different_inputs_bool(self, array_length, dtype, reduction_op):
+  @pytest.mark.parametrize("algorithm", ["ring", "recursivedoubling"])
+  def test_array_algorithms_bool(self, array_length, dtype, reduction_op, algorithm):
     input_array = np.empty(shape=(array_length), dtype=dtype)
     input_array.fill( pygpi.get_rank() > 0 )
 
@@ -50,7 +33,27 @@ class TestAllreduce:
       expected_output_array.fill(all(range(pygpi.get_size())))
     elif reduction_op == pygpi.ReductionOp.OR:
       expected_output_array.fill(any(range(pygpi.get_size())))
+
     allreduce = pygpi.Allreduce(pygpi.Group(), input_array.size, reduction_op,
+                                dtype = dtype, algorithm = algorithm)
+    allreduce.start(input_array)
+    output_array = allreduce.wait_for_completion()
+
+    assert isinstance(output_array, np.ndarray)
+    assert output_array.dtype == dtype
+    assert np.array_equal(output_array, expected_output_array)
+
+  @pytest.mark.parametrize("array_length", [0, 8, 35, 67002])
+  @pytest.mark.parametrize("dtype", [np.float, np.double, np.float32, float,
+                                     np.int32, np.int16, np.int8, int])
+  def test_array_different_inputs_dtypes(self, array_length, dtype):
+    input_array = np.empty(shape=(array_length), dtype=dtype)
+    input_array.fill(pygpi.get_rank())
+
+    expected_output_array = np.empty(input_array.shape, dtype=dtype)
+    expected_output_array.fill(sum(range(pygpi.get_size())))
+
+    allreduce = pygpi.Allreduce(pygpi.Group(), input_array.size, pygpi.ReductionOp.SUM,
                                 dtype = dtype)
     allreduce.start(input_array)
     output_array = allreduce.wait_for_completion()
@@ -105,19 +108,6 @@ class TestAllreduce:
 
     allreduce = pygpi.Allreduce(pygpi.Group(), len(input_list), pygpi.ReductionOp.MAX,
                                 dtype = dtype)
-    allreduce.start(input_list)
-    output_array = allreduce.wait_for_completion()
-    assert np.array_equal(output_array, expected_output)
-
-  @pytest.mark.parametrize("list_length", [0, 1001])
-  @pytest.mark.parametrize("dtype", ["int", "float"])
-  @pytest.mark.parametrize("algorithm", ["ring", "recursivedoubling"])
-  def test_algorithms(self, list_length, dtype, algorithm):
-    input_list = [ pygpi.get_size() ] * list_length
-    expected_output = [elem * pygpi.get_size() for elem in input_list]
-
-    allreduce = pygpi.Allreduce(pygpi.Group(), list_length, pygpi.ReductionOp.SUM,
-                                dtype = dtype, algorithm = algorithm)
     allreduce.start(input_list)
     output_array = allreduce.wait_for_completion()
     assert np.array_equal(output_array, expected_output)
