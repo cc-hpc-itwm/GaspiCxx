e6f7882c3bf8207a9d4528332163132521a21761
Alexandra <carpenamarie@itwm.fraunhofer.de>
2022-02-23 18:07:22 +0100

    tests: pygpi: broadcast: reorganize tests to test all algorithms against main dtypes (`float`, `int`, `bool`)

--- tests/python/broadcast.py
+++ tests/python/broadcast.py
@@ -7,9 +7,9 @@ import string
 
 class TestBroadcast:
 
-  def execute_broadcast(self, number_elements, root, dtype, input_buffer):
+  def execute_broadcast(self, number_elements, root, dtype, input_buffer, algorithm = None):
     broadcast = pygpi.Broadcast(pygpi.Group(), number_elements, root,
-                                dtype = dtype)
+                                dtype = dtype, algorithm = algorithm)
     if pygpi.get_rank() == root:
       broadcast.start(input_buffer)
     else:
@@ -18,18 +18,22 @@ class TestBroadcast:
     return output_array
 
   @pytest.mark.parametrize("list_length", [0, 1, 23, 1000])
-  @pytest.mark.parametrize("dtype", [float, int])
-  def test_list_identical_inputs(self, list_length, dtype):
+  @pytest.mark.parametrize("dtype", [float, int, bool])
+  @pytest.mark.parametrize("algorithm", ["linear", "sendtoall"])
+  def test_algorithms(self, list_length, dtype, algorithm):
     root = 0
-    input_list = [ dtype(3.456) ] * list_length
+    input_list = [ dtype(3.456), dtype(0), dtype(-1) ] * list_length
     expected_output_array = input_list
 
     output_array = self.execute_broadcast(len(input_list), root, dtype, input_list)
     assert isinstance(output_array, np.ndarray)
     assert np.array_equal(output_array, expected_output_array)
 
+
   @pytest.mark.parametrize("array_length", [0, 8, 35, 67002])
-  @pytest.mark.parametrize("dtype", [np.float, np.double, np.float32, np.int32, np.int16])
+  @pytest.mark.parametrize("dtype", [np.float, np.double, np.float32, float,
+                                     np.int32, np.int16, np.int8, int,
+                                     "int", "float"])
   def test_array_different_inputs(self, array_length, dtype):
     root = pygpi.get_size()-1
     input_array = np.array([idx + root for idx in range(array_length)], dtype = dtype)
@@ -37,6 +41,7 @@ class TestBroadcast:
 
     output_array = self.execute_broadcast(len(input_array), root, dtype, input_array)
     assert isinstance(output_array, np.ndarray)
+    assert output_array.dtype == dtype
     assert np.array_equal(output_array, expected_output_array)
 
   def test_single_value(self):
@@ -49,19 +54,3 @@ class TestBroadcast:
     assert isinstance(output_array, np.ndarray)
     assert len(output_array) == 1
     assert expected_output == output_array[0]
-
-  @pytest.mark.parametrize("list_length", [0, 1001])
-  @pytest.mark.parametrize("dtype", ["int", "long"])
-  @pytest.mark.parametrize("algorithm", ["linear", "sendtoall"])
-  def test_algorithms(self, list_length, dtype, algorithm):
-    root = 0
-    input_list = [ pygpi.get_size() ] * list_length
-    expected_output = input_list
-    if pygpi.get_rank() != root:
-      input_list = None
-
-    broadcast = pygpi.Broadcast(pygpi.Group(), list_length, root = 0,
-                                dtype = dtype, algorithm = algorithm)
-    broadcast.start(input_list)
-    output_array = broadcast.wait_for_completion()
-    assert np.array_equal(output_array, expected_output)
